1449525e5788b82e8c58a7e9a89b2bdf82d412e0
unknown <qj_joy@yahoo.com>
2023-09-08 07:52:43 -0400

    added json server

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,34 +2,35 @@ import {motion, AnimatePresence} from 'framer-motion';
 import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem';
 import FeedbackContext from '../context/FeedbackContext';
+import Spinner from './shared/Spinner';
 
 function FeedbackList(){
-    const {feedback} = useContext(FeedbackContext);
+    const {feedback, isLoading} = useContext(FeedbackContext);
 
-    if(!feedback || feedback.length === 0){
+    if(!isLoading && (!feedback || feedback.length === 0)){
         return <p>No Feedback Yet</p>
     }
 
-    return( 
+    return isLoading ?  (<Spinner/>) : (
         <AnimatePresence>
-            <div className='feedback-list'>
-                {feedback.map((item) => (
-                    <motion.div 
-                        key={item.id}
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        exit={{opacity: 0}}
-                    >
-                        <FeedbackItem 
-                            key={item.id} 
-                            item={item}
-                        />
-                    </motion.div>
-                ))}
-            </div>
-        </AnimatePresence>
-    )
-
+        <div className='feedback-list'>
+            {feedback.map((item) => (
+                <motion.div 
+                    key={item.id}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                >
+                    <FeedbackItem 
+                        key={item.id} 
+                        item={item}
+                    />
+                </motion.div>
+            ))}
+        </div>
+    </AnimatePresence>
+    );
+  
     // return <div className='feedback-list'>
     //     {feedback.map((item) => (
     //         <FeedbackItem 
